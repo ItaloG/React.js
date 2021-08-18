71eaf23f13190c0168bcccf1432efe0fdf398570
italo gabriel <italog03@gmail.com>
2021-08-18 11:14:04 -0300

    Paginade feed completa

---
+++ src/components/Comments/index.js
@@ -0,0 +1,24 @@
+import { Container } from "./styles";
+
+
+function Comments() {
+
+    return (
+        <Container>
+            <header>
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+
+                <strong>
+                    por Fulano de Tal
+                </strong>
+                <p>
+                    em 18/08/2021 às 8:23
+                </p>
+
+            </header>
+            <p>Este é o comentário</p>
+        </Container>
+    );
+}
+
+export default Comments;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,59 @@
 import Header from "../../components/Header";
+import Comments from "../../components/Comments";
+import { FeedContainer, GistIcon, Main, Post } from "./styles";
 
 function Home() {
 
     return (
         <>
             <Header />
-            <main>
+            <Main>
                 <nav>
-
+                    Profile
                 </nav>
-                <section>
-
-                </section>
+                <FeedContainer>
+                    <PostCard />
+                </FeedContainer>
                 <aside>
-
+                    Actions
                 </aside>
-            </main>
+            </Main>
         </>
+    );
+}
+
+function PostCard() {
+    return (
+        <Post>
+            <header>
+                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+                <div>
+                    <strong>
+                        por Fulano de Tal
+                    </strong>
+                    <p>
+                        em 18/08/2021 às 8:23
+                    </p>
+                </div>
+                <GistIcon />
+            </header>
+            <main>
+                <div>
+                    <h1>Titulo</h1>
+                    <p>Descrição da imagem bla bla bla</p>
+                </div>
+                <img src="https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp" />
+                <section>
+                    <p>Front-end</p>
+                    <p>CSS</p>
+                    <p>JS</p>
+                </section>
+            </main>
+            <footer>
+                <h2>Cometários</h2>
+                <Comments />
+            </footer>
+        </Post>
     );
 }
 
