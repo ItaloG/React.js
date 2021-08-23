a1817a043121422cadab8cb110eaf7ab250f2741
ITALO GABRIEL MESQUITA PEREI <20272538@cfp127aluno.local>
2021-08-23 11:29:44 -0300

    Preparando para consumir api

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,7 +1,7 @@
 import { Container } from "./styles";
 
 
-function Comments() {
+function Comments({ coment }) {
 
     return (
         <Container>
@@ -9,14 +9,14 @@ function Comments() {
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
 
                 <strong>
-                    por Fulano de Tal
+                    por {coment.author.name}
                 </strong>
                 <p>
-                    em 18/08/2021 às 8:23
+                    em {coment.crated_at} às 8:23
                 </p>
 
             </header>
-            <p>Este é o comentário</p>
+            <p>{coment.description}</p>
         </Container>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,28 @@ import { FeedContainer, GistIcon, Main, Post } from "./styles";
 
 function Home() {
 
+    const posts = [{
+        author: {
+            name: "Fulano",
+        },
+        crated_at: "10/10/2021",
+        title: "Este é um post sobre js",
+        image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        gist: "https://githuv.com.br/",
+        categories: [
+            "JS", "Back-end", "Express"
+        ],
+        coments: [
+            {
+                author: {
+                    name: "ciclano",
+                },
+                created_at: "11/10/2021",
+                description: "Realmente Js é muito legal"
+            }
+        ]
+    }]
+
     return (
         <>
             <Header />
@@ -12,7 +34,7 @@ function Home() {
                     Profile
                 </nav>
                 <FeedContainer>
-                    <PostCard />
+                    {posts.map(post => < PostCard post={post}/>)}
                 </FeedContainer>
                 <aside>
                     Actions
@@ -22,36 +44,34 @@ function Home() {
     );
 }
 
-function PostCard() {
+function PostCard({ post }) {
     return (
         <Post>
             <header>
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                 <div>
                     <strong>
-                        por Fulano de Tal
+                        por {post.author.name}
                     </strong>
                     <p>
-                        em 18/08/2021 às 8:23
+                        {post.created_at}
                     </p>
                 </div>
-                <GistIcon />
+                {post.gist && <GistIcon />}
             </header>
             <main>
                 <div>
-                    <h1>Titulo</h1>
-                    <p>Descrição da imagem bla bla bla</p>
+                    <h1>{post.title}</h1>
+                    <p>{post.description}</p>
                 </div>
                 <img src="https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp" />
                 <section>
-                    <p>Front-end</p>
-                    <p>CSS</p>
-                    <p>JS</p>
+                    {post.categories.map(category => <p>{category}</p>)}
                 </section>
             </main>
             <footer>
-                <h2>Cometários</h2>
-                <Comments />
+                <h2>Comentários</h2>
+                {post.coments.map(coment => <Comments coment={coment} />)}
             </footer>
         </Post>
     );
